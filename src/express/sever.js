834fc4bccbd7131e99cf0d54d31811b29f60c835
WangFan <60833707+coderWangFan@users.noreply.github.com>
2022-06-26 12:09:12 +0800

    feat(post请求)

--- src/express/sever.js
+++ src/express/sever.js
@@ -5,12 +5,26 @@ const express=require('express')
 const app=express()
 
 //创建路由规则
+
+
+//get请求
 app.get('/sever',(request,response)=>{
     //设置响应头，跨域
     response.setHeader('Access-Control-Allow-Origin','*')
 
     //设置响应
-    response.send('hello express--sever')
+    response.send('hello express--get请求')
+})
+
+app.post('/sever',(request,response)=>{
+    //设置响应头，跨域
+    response.setHeader('Access-Control-Allow-Origin','*')
+
+    //设置响应头，允许自定义请求头信息
+    response.setHeader('Access-Control-Allow-Headers','*')
+
+    //设置响应
+    response.send('hello express--post请求')
 })
 
 //监听端口，启动服务
