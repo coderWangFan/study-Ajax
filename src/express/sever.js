014f85d4d7e82ee393c2bc5d827ae7c0a894bd84
WangFan <60833707+coderWangFan@users.noreply.github.com>
2022-06-28 10:34:48 +0800

    feat(json格式返回)

--- src/express/sever.js
+++ src/express/sever.js
@@ -27,6 +27,23 @@ app.post('/sever',(request,response)=>{
     response.send('hello express--post请求')
 })
 
+
+app.all('/json-sever',(request,response)=>{
+    //设置响应头，跨域
+    response.setHeader('Access-Control-Allow-Origin','*')
+
+    //设置响应头，允许自定义请求头信息
+    response.setHeader('Access-Control-Allow-Headers','*')
+
+    const data={
+        name:'小王',
+        task:'测试json返回'
+    }
+
+    //设置响应  send不支持对象格式
+    response.send(JSON.stringify(data))
+})
+
 //监听端口，启动服务
 app.listen(8000,()=>{
     console.log('8000端口开启了，正在进行监听中')
